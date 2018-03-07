8d366c76ba621b5fbf3629807707347ef38e8b78
Kevin <kjh7796@gmail.com>
2018-03-06 20:15:45 -0500

    UI and error log changes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ window.onload = function() {
   //var frame = "<form action=''/file-upload' class='dropzone' id='dropzone'> Drag File Here </form>"
   var form = document.createElement("form");
   var script = document.createElement("script");
+  var progress = document.createElement("progress")
+  progress.setAttribute("id", "progress");
+  progress.setAttribute("value", "0");
+  progress.setAttribute("max", "100");
   script.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/dropzone.js");
   script.setAttribute("type", "text/javascript");
   form.setAttribute("class", "dropzone");
@@ -12,6 +16,7 @@ window.onload = function() {
   document.getElementById("url-field").style.cssText = "width: 500px !important; text-align: center !important;";
 
   document.getElementById("url-field").appendChild(form);
+  document.getElementById("url-field").appendChild(progress);
   document.body.appendChild(script);
   document.getElementById("dropzone").style.cssText = "border-radius: 5px !important ;width: 500px !important; height: 355px !important; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19) !important; background-color: white !important;";
 
@@ -23,12 +28,18 @@ window.onload = function() {
      document.getElementById("url").value = `${link}`;
     }); //Print the link inside the imgur json response, do the same with gfycat
    this.on("error", function(file, err){
-     document.getElementById("status").innerHTML = err.data.error.message;
-     document.getElementById("status").style.cssText = "color: red;";
+     document.getElementById("url").value = "ERROR CHECK CONSOLE";
+     document.getElementById("url").style.cssText = "color: red !important;";
+     //progress.style.cssText = "background-color: red !important;"
+
+     if(err.data.error.message == undefined){
+       console.error(`UPLOAD ERROR: ${err.data.error}`)
+     }else{console.error(`UPLOAD ERROR: ${err.data.error.message}`);}
+
  });
-   //this.on("uploadprogress", function(file, prog){console.log(prog);});
+   this.on("totaluploadprogress", function(totalprogress){progress.setAttribute("value", totalprogress);});
  },
-   url: "https://api.imgur.com/3/image",
+   url: "https://api.imgur.com/3/upload",
    headers: {
    'Authorization': 'Client-ID e5df71c6267ac42',
    'Accept': 'application/json',
